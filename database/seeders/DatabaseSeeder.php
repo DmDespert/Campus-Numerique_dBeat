f0e0cba96b97ecb3dc23b1d66819c3b505024484
Dimitri <dimitri.despert@gmail.com>
2021-02-26 16:21:16 +0100

    feat: add foreignkeys for categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
            ProductSeeder::class
         ]);
+        $this->call([
+            CategorySeeder::class
+        ]);
     }
 }
