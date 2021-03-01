006538d1b754b2ffd933859071c52d381fb5ecfe
MatiRoss <mati04.ross@gmail.com>
2021-03-01 11:39:07 +0100

    feat: Show cart

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,8 @@ Route::get('/product', function () {
 Route::get('/product/{id}',
     [ProductController::class, 'show']
 );
+
+Route::get('/cart',
+    [CartController::class, 'index']
+);
+
